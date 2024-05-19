943628e6b13957870a677ad04c9264ba66e562b6
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-05-19 10:43:47 -0500

    static carpeta

--- main/application.py
+++ main/application.py
@@ -3,6 +3,10 @@
 from flask import Flask
 from .blueprints import register
 
-APP = Flask(__name__)
+APP = Flask(
+  __name__,
+  static_folder='../static',
+  static_url_path='/'
+)
 
 register(APP)
