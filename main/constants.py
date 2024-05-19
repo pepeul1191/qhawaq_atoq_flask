a4da8d47e50ff2ddbcf89e8a7aaa59b6a9b711af
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-05-19 11:48:21 -0500

    contants en funcion a un .env

--- main/constants.py
+++ main/constants.py
@@ -1,20 +1,23 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+ENV = os.getenv('ENV')
 
-CONSTANTS = {
-  'base_url': 'http://localhost:5000/',
-  'static_url': 'http://localhost:5000/',
-  'sistema_id': '3',
-  'csrf': {
-      'key': 'csrftoken',
-      'secret': 'holamundojejejojo18927389127389123'
-  },
-  'ambiente_static': 'desarrollo',
-  'ambiente_session': 'inactivo',
-  'ambiente_csrf': 'activo',
-  'ambiente_500': 'activo',
-  'ambiente_error_handler': 'inactivo',
-  'key': 'Fdp6CVxpri1ga8kH',
-  'default_language': 'sp',
-}
+if ENV == 'local':
+  CONSTANTS = {
+    'base_url': 'http://localhost:5000/',
+    'static_url': 'http://localhost:5000/',
+  }
+elif ENV == 'ulima':
+  CONSTANTS = {
+    'base_url': 'http://ulima.edu.pe/',
+    'static_url': 'http://localhost:5000/',
+  }
+else:
+  CONSTANTS = {
+    'base_url': 'http://ulima.edu.pex/',
+    'static_url': 'http://localhost:5000/',
+  }
