326d15afe9dab5b9c999b7a32e7d9484e0223910
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-05-19 10:42:17 -0500

    renderizando una vista jinja

--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 view = Blueprint('main', __name__)
 
 @view.route('/', methods=['GET'])
 def home():
-  return 'Home'
+  return render_template('home.html')
+
+@view.route('/error/access/<code>', methods=['GET'])
+def error_access(code):
+  # lang = session_language(session)
+  # locals_dic = { }
+  # return render_template('demo/index.html', locals=locals_dic)
+  return 'Recurso no encontrado', code
