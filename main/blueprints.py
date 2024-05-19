34d81908f3e0fdc4c3ef0a8fa847be7eaff2a2e1
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-05-19 12:17:07 -0500

    correcion de carpetas

--- main/blueprints.py
+++ main/blueprints.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .views import view as main_blueprints
-from .demo.blueprints import blueprints as demo_blueprints
+from demo.blueprints import blueprints as demo_blueprints
 
 def register(app):
+  # append sub blueprints
   modules_blueprints = []
   modules_blueprints.append(demo_blueprints)
-  # cargar blueprints a app
+  # load main blueprint to app
+  app.register_blueprint(main_blueprints)
+  # load sub blueprints to app
   for blueprints in modules_blueprints:
     for blueprint in blueprints:
       app.register_blueprint(blueprint)
-  app.register_blueprint(main_blueprints)
   # register oauth
   # app.register_blueprint(oauth_view)
   # registar error/access/:code
